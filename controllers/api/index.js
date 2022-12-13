6a2c42ce306be300272d18f5f57a683fee4ee3d8
ADeekollu <115489389+ADeekollu@users.noreply.github.com>
2022-12-13 17:09:44 -0500

    changed naming conventions to match file names

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
-const commentRoute = require('./comment');
+const commentRoute = require('./comment-route');
 
 router.use('/users', userRoutes);
-router.use('/comment', commentRoute)
+router.use('/comment-route', commentRoute)
 
 module.exports = router;
