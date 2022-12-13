6081e4f092cf81b4b566bedfb88de41c8ad19dd7
Yosef Teshome <menelik08@gmail.com>
2022-12-13 12:00:44 -0500

    comment table linking progress

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Brand = require('./Brand');
 const Style = require('./Style');
+const Comment = require('./Comment');
 
 Brand.hasMany(Style, {
   foreignKey: 'brand_id',
@@ -10,4 +11,11 @@ Style.belongsTo(Brand, {
   foreignKey: 'brand_id',
 });
 
-module.exports = { User, Brand, Style };
+Style.hasMany(Comment, {
+  foreignKey: 'style_id',
+})
+
+Comment.belongsTo(Style, {
+  foreignKey: 'style_id',
+})
+module.exports = { User, Brand, Style, Comment };
